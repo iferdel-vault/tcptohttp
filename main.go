312c882f0c92a86b5fb2d2efb722d10f0163dfb4
iferdel <ignaferdel@gmail.com>
2025-07-21 20:50:48 -0400

    channel implementation

--- main.go
+++ main.go
@@ -21,31 +21,44 @@ func main() {
 	fmt.Printf("Reading data from %s\n", filename)
 	fmt.Println("=====================================")
 
+	ch := getLinesChannel(file)
+
+	for elem := range ch {
+		fmt.Printf("read: %s\n", elem)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	c := make(chan string)
 	b := make([]byte, 8, 8)
 	var line string
-	for {
-		n, err := file.Read(b)
-		if err != nil {
-			if line != "" {
-				fmt.Printf("read: %s\n", line)
-				line = ""
-			}
-			if errors.Is(err, io.EOF) {
+	go func(c chan<- string) {
+		for {
+			n, err := f.Read(b)
+			if err != nil {
+				if line != "" {
+					c <- fmt.Sprintf("%s", line)
+					line = ""
+				}
+				if errors.Is(err, io.EOF) {
+					close(c)
+					break
+				}
+				log.Fatalf("error reading file: %v", err)
 				break
 			}
-			log.Fatalf("error reading file: %v", err)
-			break
-		}
-		str := string(b[:n])
-		parts := strings.Split(str, "\n")
-		if len(parts) > 1 {
-			for i := 0; i < len(parts)-1; i++ {
-				line += parts[i]
+			str := string(b[:n])
+			parts := strings.Split(str, "\n")
+			if len(parts) > 1 {
+				for i := 0; i < len(parts)-1; i++ {
+					line += parts[i]
+				}
+				c <- fmt.Sprintf("%s", line)
+				line = "" + parts[len(parts)-1]
+			} else {
+				line += parts[0]
 			}
-			fmt.Printf("read: %s\n", line)
-			line = "" + parts[len(parts)-1]
-		} else {
-			line += parts[0]
 		}
-	}
+	}(c)
+	return c
 }
