e6e6c6ac1fd2255da688a856d87bdf3f424862f7
iferdel <ignaferdel@gmail.com>
2025-08-08 20:53:18 -0400

    chunk body write

--- internal/response/writer.go
+++ internal/response/writer.go
@@ -62,3 +62,42 @@ func (w *Writer) WriteBody(p []byte) (int, error) {
 	n, err := w.conn.Write(p)
 	return n, err
 }
+
+func (w *Writer) WriteChunkedBody(p []byte) (int, error) {
+	if w.writerState != WriterStateBody {
+		return 0, fmt.Errorf("cannot write body in state %d", w.writerState)
+	}
+	chunkSize := len(p)
+
+	nTotal := 0
+	n, err := fmt.Fprintf(w.conn, "%x\r\n", chunkSize)
+	if err != nil {
+		return nTotal, nil
+	}
+	nTotal += n
+
+	n, err = w.conn.Write(p)
+	if err != nil {
+		return nTotal, err
+	}
+	nTotal += n
+
+	n, err = w.conn.Write([]byte("\r\n"))
+	if err != nil {
+		return nTotal, err
+	}
+	nTotal += n
+
+	return nTotal, nil
+}
+
+func (w *Writer) WriteChunkedBodyDone() (int, error) {
+	if w.writerState != WriterStateBody {
+		return 0, fmt.Errorf("cannot write body in state %d", w.writerState)
+	}
+	n, err := w.conn.Write([]byte("0\r\n\r\n"))
+	if err != nil {
+		return n, err
+	}
+	return n, nil
+}
